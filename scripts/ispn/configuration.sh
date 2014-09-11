bcd81442115ee3c20ee7bad142eaab83e005064f
otrack <0track@gmail.com>
2014-09-11 14:59:01 +0200

    [ADD] several fixes

---
+++ scripts/ispn/configuration.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ../gconfiguration.sh
+
+ISPN_TMPL_FILE="../../templates/infinispan.tmpl"
+ISPN_TMPL_ID="240"
+
+
---
+++ scripts/ispn/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source configuration.sh
+
+NAME="ispn"
+NISPN="3"
+BPING_PORT="36001"
+
+# for each IP
+# correct template 
+# then instanciate vm with the appropriate name
+for i in `seq 1 ${NISPN}`
+do
+    echo "Instanciating infinispan ${i}"
+    sed s/\@BPING_PORT\@/"${BPING_PORT}"/ ${ISPN_TMPL_FILE} > tmp
+    scp tmp ${USER}@${CLUSTER}:~/
+    ${SSHCMDHEAD} onetemplate update ${ISPN_TMPL_ID} tmp
+    ${SSHCMDHEAD} onetemplate instantiate ${ISPN_TMPL_ID} --name ${NAME}
+done
+rm -f tmp
