5184b70f7c3f864673f48101db2c00d2b949e5bc
otrack <0track@gmail.com>
2014-11-12 18:39:34 +0100

    modified:   build/etc/local.d/vmcontextgentoo 	new file:   build/hadoop/04hadoop.start 	new file:   build/hadoop/04hadoop.stop 	new file:   build/hadoop/core-site.xml.tmpl 	new file:   build/hadoop/hadoop 	new file:   build/hadoop/hdfs-site.xml.tmpl 	new file:   build/hadoop/mapred-site.xml.tmpl 	new file:   build/hadoop/slaves.tmpl 	new file:   build/hadoop/yarn-site.xml.tmpl 	modified:   build/infinispan/infinispan-config.xml.tmpl 	modified:   qemu/blank.iso 	modified:   qemu/context.sh 	modified:   qemu/lqemu.sh 	modified:   scripts/ispn/deploy.sh 	modified:   templates/infinispan.tmpl

--- qemu/lqemu.sh
+++ qemu/lqemu.sh
@@ -6,9 +6,11 @@ qemu-img create -f qcow2 -b ${IMG_GENTOO} gentoo.qcow2
 qemu-img create -f qcow2 -b ${IMG_OPT} opt.qcow2
 rm -f blank.iso
 mkisofs -o blank.iso context.sh
-qemu-system-i386 gentoo.qcow2 -hdb opt.qcow2 \
+qemu-system-i386 gentoo.qcow2 -hdb opt.qcow2\
     -m 1500M \
+    -cpu qemu64,+ssse3,+sse4.1,+sse4.2,+x2apic \
+    -smp 4 \
     -cdrom blank.iso \
-    -net nic,model=virtio,macaddr=02:00:c0:a8:4f:41
+    -net nic,model=virtio,macaddr=02:00:c0:a8:4f:41 \
     -net user,hostfwd=tcp:127.0.0.1:11222-:11222
     
--- scripts/ispn/deploy.sh
+++ scripts/ispn/deploy.sh
@@ -2,16 +2,16 @@
 
 source configuration.sh
 
-NAME="ispn"
-NISPN="3"
-BPING_PORT="36001"
+NAME="ispn1"
+NISPN="6"
+BPING_PORT="26001"
 
 # for each IP
 # correct template 
 # then instanciate vm with the appropriate name
 for i in `seq 1 ${NISPN}`
 do
-    echo "Instanciating infinispan ${i}"
+    echo "Instanciating infinispan #${i} (${NAME})"
     sed s/\@BPING_PORT\@/"${BPING_PORT}"/ ${ISPN_TMPL_FILE} > tmp
     scp tmp ${USER}@${CLUSTER}:~/
     ${SSHCMDHEAD} onetemplate update ${ISPN_TMPL_ID} tmp
